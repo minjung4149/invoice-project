d3b236ac67429df1744f7235d32dd93a703dc0ed
“MinjungSeo” <mjseo@notiplus.co.kr>
2025-04-10 16:23:48 +0900

    빌드에러 해결

--- src/app/client-detail/edit-invoice/page.tsx
+++ src/app/client-detail/edit-invoice/page.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import {Suspense} from "react";
 import EditInvoiceClient from "@/components/clientDetail/EditInvoiceClient";
+import HeaderDetailClient from "@/components/header/HeaderDetailClient";
+
+export const dynamic = 'force-dynamic';
 
 const EditInvoicePage = () => {
-  return <EditInvoiceClient />;
+  return (
+    <>
+      <Suspense fallback={<p>헤더 로딩 중...</p>}>
+        <HeaderDetailClient/>
+      </Suspense>
+
+      <Suspense fallback={<p>수정 페이지 로딩 중...</p>}>
+        <EditInvoiceClient/>
+      </Suspense>
+    </>
+  );
 };
 
 export default EditInvoicePage;
