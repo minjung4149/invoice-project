7a95c8de045affd78f469ee06c864b18b8065e22
“MinjungSeo” <mjseo@notiplus.co.kr>
2025-04-30 09:52:50 +0900

    Prisma Client 인스턴스를 싱글톤(Singleton) 패턴으로 관리

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,12 @@
-import {PrismaClient} from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
