7aa7f16fb5f600c577b5aa3d3a9c3483ab4cec6b
“MinjungSeo” <mjseo@notiplus.co.kr>
2025-05-21 10:57:06 +0900

    에러 해결

--- src/app/api/invoice/findById/route.ts
+++ src/app/api/invoice/findById/route.ts
@@ -25,7 +25,7 @@ export async function GET(req: Request) {
           select: {name: true, phone: true},
         },
         details: {
-          select: {name: true, quantity: true, price: true},
+          select: {name: true, spec: true, quantity: true, price: true},
         },
       },
     });
